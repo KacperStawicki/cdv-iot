dfce1fd280c2f40facdfbde672efcaf1d248d18b
michal <michuaepl@gmail.com>
2025-04-09 17:53:00 +0200

    Dodanie basic emulatora

---
+++ UkladSymulacja/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
+void app_main(void)
+{
+
+    
+}
