09c3a2f61fa5197c88ae6937fa98b458a1f94db3
michal <michuaepl@gmail.com>
2025-04-15 20:17:38 +0200

    basic skrypt symulacji układu

---
+++ SymulacjaUklad/Clients/HttpClient.py
@@ -0,0 +1,12 @@
+import requests
+import Singletons.singletons as singletons
+
+class HttpClient:
+    def __init__(self):
+        pass
+
+    def GetWetnessStatus(self):
+        pass
+
+    def SendWetnessState(self):
+        pass
---
+++ SymulacjaUklad/main.py
@@ -0,0 +1,9 @@
+import Clients.HttpClient as HttpClient
+from Singletons.singletons import Settings
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
