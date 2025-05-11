21a2f66f10eeed30d0cf11bb8f80cbef9544986d
Kacper Stawicki <stawicki.k02@gmail.com>
2025-05-11 16:14:13 +0200

    Remove device routes implementation from index.ts, including device registration, claiming, and retrieval logic. This cleanup eliminates unused code and streamlines the backend structure.

---
+++ backend/src/routes/device/list/index.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { TokenDecoded } from '../../../utils/types';
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+
+const listDevices: FastifyPluginAsyncZod = async (fastify): Promise<void> => {
+  const prisma = new PrismaClient();
+
+  fastify.get(
+    '',
+    {
+      schema: {
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string(),
+              name: z.string().nullable(),
+              claimed: z.boolean(),
+              thresholdRed: z.number(),
+              thresholdYellow: z.number(),
+              thresholdGreen: z.number(),
+            })
+          ),
+        },
+        tags: ['Device'],
+        summary: 'Get all devices',
+        description: 'Get all devices for the authenticated user',
+      },
+      preHandler: [fastify.authenticate],
+    },
+    async (request) => {
+      // Get authenticated user
+      const userId = request.user as TokenDecoded;
+
+      const devices = await prisma.device.findMany({
+        where: { userId: userId.id },
+        select: {
+          id: true,
+          name: true,
+          claimed: true,
+          thresholdRed: true,
+          thresholdYellow: true,
+          thresholdGreen: true,
+        },
+      });
+
+      return devices;
+    }
+  );
+};
+
+export default listDevices;
